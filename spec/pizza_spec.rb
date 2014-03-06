98930fd094707120a13c1119ed5495fa2d118aa3
Brady Bryan <jbraden.bryan@gmail.com>
2014-03-06 11:43:53 -0600

    adds failing initialize method for Pizza class

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,6 +4,17 @@ describe Pizza do
   it "exists" do
     expect(Pizza).to be_a(Class)
   end
+
+  describe "initialize" do
+    it "records all of the toppings" do
+      toppings = [
+        Topping.new("pepperoni"),
+        Topping.new("onion", vegetarian:true)
+      ]
+      pie = Pizza.new(toppings)
+      expect(pie.toppings).to eq(toppings)
+    end
+  end
 end
 
 describe Topping do
@@ -18,8 +29,10 @@ end
     expect(topping.name).to eq("pep")
     end
     it "identifies a topping as vegetarian or not" do
-      topping = Topping.new("pep", vegetarian: false)
+      topping = Topping.new("pep")
+      topping2 = Topping.new("onion", vegetarian: true)
     expect(topping.vegetarian).to eq(false)
+    expect(topping2.vegetarian).to eq(true)
   end
 end
 
